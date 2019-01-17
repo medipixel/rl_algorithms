d9f3abad1ea5689f0976d04ce3cd00227370ab95
curt <curt.park@medipixel.io>
2019-01-17 17:40:41 +0900

    Do refactoring reinforce

--- baselines/reinforce/model.py
+++ baselines/reinforce/model.py
@@ -8,58 +8,13 @@ with continuous action space in OpenAI Gym.
 - Contact: curt.park@medipixel.io
 """
 
-import argparse
-import gym
-
 import torch
 import torch.nn as nn
 from torch.distributions import Normal
 
-from reinforce_agent import ReinforceAgent
 
-
-# configurations
-parser = argparse.ArgumentParser(description='Reinforce with continuous action\
-                                             example by Pytorch')
-parser.add_argument('--gamma', type=float, default=0.99,
-                    help='discount factor')
-parser.add_argument('--std', type=float, default=1.0,
-                    help='standard deviation for normal distribution')
-parser.add_argument('--seed', type=int, default=777,
-                    help='random seed for reproducibility')
-parser.add_argument('--env', type=str, default='LunarLanderContinuous-v2',
-                    help='openai gym environment name\
-                          (continuous action only)')
-parser.add_argument('--max-episode-steps', type=int, default=500,
-                    help='max steps per episode')
-parser.add_argument('--episode-num', type=int, default=1500,
-                    help='total episode number')
-parser.add_argument('--model-path', type=str,
-                    help='load the saved model and optimizer at the beginning')
-parser.add_argument('--render-after', type=int, default=0,
-                    help='start rendering after the input number of episode')
-parser.add_argument('--no-render', dest='render', action='store_false',
-                    help='turn off rendering')
-parser.set_defaults(render=True)
-parser.set_defaults(model_path=None)
-args = parser.parse_args()
-
-
-# initialization
-env = gym.make(args.env)
-env._max_episode_steps = args.max_episode_steps
-state_dim = env.observation_space.shape[0]
-action_dim = env.action_space.shape[0]
-action_low = float(env.action_space.low[0])
-action_high = float(env.action_space.high[0])
-
-# set random seed
-env.seed(args.seed)
-torch.manual_seed(args.seed)
-
-
-class ReinforceContinuousAction(nn.Module):
-    """Reinforce continuous action model with simple FC layers.
+class ActorCritic(nn.Module):
+    """Reinforce continuous actor-critic model with simple FC layers.
 
     Args:
         state_dim (int): dimension of state space
@@ -73,12 +28,16 @@ class ReinforceContinuousAction(nn.Module):
 
     """
 
-    def __init__(self, state_dim, action_dim):
+    def __init__(self, std, state_dim, action_dim, action_low, action_high):
         """Initialization."""
-        super(ReinforceContinuousAction, self).__init__()
+        super(ActorCritic, self).__init__()
 
         self.state_dim = state_dim
         self.action_dim = action_dim
+        self.action_low = action_low
+        self.action_high = action_high
+
+        self.std = std
         self.actor_mu = nn.Sequential(
                             nn.Linear(self.state_dim, 24),
                             nn.ReLU(),
@@ -119,16 +78,11 @@ class ReinforceContinuousAction(nn.Module):
 
         """
         norm_dist_mu = self.actor_mu(state)
-        norm_dist_std = args.std
+        norm_dist_std = self.std
         predicted_value = self.critic(state)
 
         dist = Normal(norm_dist_mu, norm_dist_std)
-        selected_action = torch.clamp(dist.rsample(), action_low, action_high)
+        selected_action = torch.clamp(dist.rsample(),
+                                      self.action_low, self.action_high)
 
         return selected_action, predicted_value, dist
-
-
-if __name__ == '__main__':
-    model = ReinforceContinuousAction(state_dim, action_dim)
-    agent = ReinforceAgent(env, model, args)
-    agent.run()
