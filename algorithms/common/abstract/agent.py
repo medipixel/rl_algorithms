Add sacfd and fix fd priority calculation (#58)
* Add sacfd and fix fd priority calculation

Squashed commit of the following:

commit 95b8de77637495d3c98550f6bd5d5bd300c390a6
Author: curt <curt.park@medipixel.io>
Date:   Wed Jan 30 20:38:52 2019 +0900

    Add sacfd

* Chage pretraining step to 0

* Modify unnecessary squeeze lines

algorithms/common/abstract/agent.py
@@ -7,10 +7,10 @@
 
 import argparse
 import os
+import subprocess
 from abc import ABC, abstractmethod
 from typing import Tuple
 
-import git
 import gym
 import numpy as np
 import torch
@@ -47,8 +47,11 @@ class AbstractAgent(ABC):
         self.action_dim = env.action_space.shape[0]
 
         # for logging
-        repo = git.Repo(search_parent_directories=True)
-        self.sha = repo.head.object.hexsha[:7]
+        self.sha = (
+            subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])[:-1]
+            .decode("ascii")
+            .strip()
+        )
 
     @abstractmethod
     def select_action(self, state: np.ndarray):