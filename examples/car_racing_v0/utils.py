b6bc711eed77390c25300543b5c0ce44256dc6cb
curt-park <curt.park@medipixel.io>
2019-02-21 21:13:42 +0900

    Remove skimage

--- examples/car_racing_v0/utils.py
+++ examples/car_racing_v0/utils.py
@@ -7,11 +7,10 @@
 
 import cv2
 import numpy as np
-from skimage import color
 
 
 def process_image(obs: np.ndarray):
     """Preprocessing function."""
-    gray_obs = 2 * color.rgb2gray(obs) - 1.0
+    gray_obs = 2 * cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY) - 1.0
     gray_obs_resized = cv2.resize(gray_obs, dsize=(64, 64))
     return gray_obs_resized.reshape(-1, 64, 64)
