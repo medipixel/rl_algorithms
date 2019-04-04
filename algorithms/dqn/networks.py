Fix loss calculation error

algorithms/dqn/networks.py
@@ -149,7 +149,7 @@ class C51DuelingMLP(MLP):
         return q
 
 
-class IQNDuelingMLP(MLP):
+class IQNMLP(MLP):
     """Multilayered perceptron for IQN with dueling construction.
 
     Reference: https://github.com/google/dopamine
@@ -166,16 +166,17 @@ class IQNDuelingMLP(MLP):
         init_w: float = 3e-3,
     ):
         """Initialization."""
-        super(IQNDuelingMLP, self).__init__(
+        IQNMLP.n_quantiles = n_quantiles
+        self.quantile_embedding_dim = quantile_embedding_dim
+        self.input_size = input_size
+        self.output_size = output_size
+
+        super(IQNMLP, self).__init__(
             input_size=input_size,
             output_size=output_size,
             hidden_sizes=hidden_sizes,
             hidden_activation=hidden_activation,
         )
-        IQNDuelingMLP.n_quantiles = n_quantiles
-        self.quantile_embedding_dim = quantile_embedding_dim
-        self.input_size = input_size
-        self.output_size = output_size
 
         # set quantile_net layer
         self.quantile_fc_layer = nn.Linear(self.quantile_embedding_dim, self.input_size)
@@ -204,7 +205,7 @@ class IQNDuelingMLP(MLP):
         # Hadamard product
         quantile_net = state_tiled * quantile_net
 
-        quantile_values = super(IQNDuelingMLP, self).forward(quantile_net)
+        quantile_values = super(IQNMLP, self).forward(quantile_net)
 
         return quantile_values, quantiles
 
@@ -222,6 +223,6 @@ class IQNDuelingMLP(MLP):
     def __get_n_tau_samples(n_tau_samples: Optional[int]) -> int:
         """Get sample tau number."""
         if not n_tau_samples:
-            return IQNDuelingMLP.n_quantiles
+            return IQNMLP.n_quantiles
         else:
             return n_tau_samples

algorithms/dqn/utils.py
@@ -13,14 +13,14 @@ import torch
 import torch.nn.functional as F
 
 from algorithms.common.networks.mlp import MLP
-from algorithms.dqn.networks import C51DuelingMLP, IQNDuelingMLP
+from algorithms.dqn.networks import IQNMLP, C51DuelingMLP
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 def calculate_iqn_loss(
-    model: IQNDuelingMLP,
-    target_model: IQNDuelingMLP,
+    model: IQNMLP,
+    target_model: IQNMLP,
     experiences: Tuple[torch.Tensor, ...],
     gamma: float,
     batch_size: int,
@@ -117,7 +117,7 @@ def calculate_iqn_loss(
     # Sum over current quantile value (n_tau_samples) dimension,
     # average over target quantile value (n_tau_prime_samples) dimension.
     # Shape: batch_size x n_tau_prime_samples x 1.
-    loss = torch.mean(quantile_huber_loss, dim=2)
+    loss = torch.sum(quantile_huber_loss, dim=2)
 
     # Shape: batch_size x 1.
     iqn_loss_element_wise = torch.mean(loss, dim=1)

examples/lunarlander_v2/dqfd.py
@@ -11,7 +11,7 @@ import gym
 import torch
 import torch.optim as optim
 
-from algorithms.dqn.networks import DuelingMLP, IQNDuelingMLP
+from algorithms.dqn.networks import IQNMLP, DuelingMLP
 from algorithms.fd.dqn_agent import Agent
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -22,7 +22,7 @@ hyper_params = {
     "GAMMA": 0.99,
     "TAU": 5e-3,
     "BUFFER_SIZE": int(1e5),
-    "BATCH_SIZE": 64,
+    "BATCH_SIZE": 32,
     "LR_DQN": 1e-4,  # dueling: 6.25e-5
     "ADAM_EPS": 1e-8,  # rainbow: 1.5e-4
     "MAX_EPSILON": 1.0,
@@ -40,7 +40,7 @@ hyper_params = {
     "MARGIN": 0.8,  # margin for supervised loss
     "GRADIENT_CLIP": 0.5,
     "UPDATE_STARTS_FROM": int(1e3),
-    "TRAIN_FREQ": 8,
+    "TRAIN_FREQ": 4,
     "MULTIPLE_LEARN": 1,
     # Distributional Q function
     "USE_DIST_Q": True,
@@ -64,10 +64,10 @@ def run(env: gym.Env, args: argparse.Namespace, state_dim: int, action_dim: int)
     """
     # create model
     def get_fc_model():
-        hidden_sizes = [128, 64]
+        hidden_sizes = [512, 128]
 
         if hyper_params["USE_DIST_Q"]:
-            model = IQNDuelingMLP(
+            model = IQNMLP(
                 input_size=state_dim,
                 output_size=action_dim,
                 hidden_sizes=hidden_sizes,

examples/lunarlander_v2/dqn.py
@@ -12,7 +12,7 @@ import torch
 import torch.optim as optim
 
 from algorithms.dqn.agent import Agent
-from algorithms.dqn.networks import DuelingMLP, IQNDuelingMLP
+from algorithms.dqn.networks import IQNMLP, DuelingMLP
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -22,9 +22,9 @@ hyper_params = {
     "GAMMA": 0.99,
     "TAU": 5e-3,
     "W_N_STEP": 1.0,
-    "W_Q_REG": 0,  # 1e-7,
+    "W_Q_REG": 1e-7,
     "BUFFER_SIZE": int(1e5),
-    "BATCH_SIZE": 64,
+    "BATCH_SIZE": 32,
     "LR_DQN": 1e-4,  # dueling: 6.25e-5
     "ADAM_EPS": 1e-8,  # rainbow: 1.5e-4
     "WEIGHT_DECAY": 1e-7,
@@ -60,10 +60,10 @@ def run(env: gym.Env, args: argparse.Namespace, state_dim: int, action_dim: int)
     """
     # create model
     def get_fc_model():
-        hidden_sizes = [128, 64]
+        hidden_sizes = [512, 128]
 
         if hyper_params["USE_DIST_Q"]:
-            model = IQNDuelingMLP(
+            model = IQNMLP(
                 input_size=state_dim,
                 output_size=action_dim,
                 hidden_sizes=hidden_sizes,

examples/pong_no_frameskip_v4/dqn.py
@@ -13,7 +13,7 @@ import torch.optim as optim
 
 from algorithms.common.networks.cnn import CNN, CNNLayer
 from algorithms.dqn.agent import Agent
-from algorithms.dqn.networks import DistCNN, DuelingMLP, IQNDuelingMLP
+from algorithms.dqn.networks import IQNMLP, DistCNN, DuelingMLP
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -66,7 +66,7 @@ def run(env: gym.Env, env_name: str, args: argparse.Namespace):
 
         if hyper_params["USE_DIST_Q"]:
             Model = DistCNN
-            fc_model = IQNDuelingMLP(
+            fc_model = IQNMLP(
                 input_size=fc_input_size,
                 output_size=action_dim,
                 hidden_sizes=hidden_sizes,