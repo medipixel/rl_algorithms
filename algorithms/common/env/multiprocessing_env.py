56a7eb05397dd63aed912ed4132330570fc0aefc
khkim <kh.kim@medipixel.io>
2020-01-02 19:05:52 +0900

    Modify multiprocessing_env because of linting

--- algorithms/common/env/multiprocessing_env.py
+++ algorithms/common/env/multiprocessing_env.py
@@ -23,9 +23,9 @@ def tile_images(img_nhwc):
     H = int(np.ceil(np.sqrt(N)))
     W = int(np.ceil(float(N) / H))
     img_nhwc = np.array(list(img_nhwc) + [img_nhwc[0] * 0 for _ in range(N, H * W)])
-    img_HWhwc = img_nhwc.reshape(H, W, h, w, c)
+    img_HWhwc = img_nhwc.reshape((H, W, h, w, c))
     img_HhWwc = img_HWhwc.transpose(0, 2, 1, 3, 4)
-    img_Hh_Ww_c = img_HhWwc.reshape(H * h, W * w, c)
+    img_Hh_Ww_c = img_HhWwc.reshape((H * h, W * w, c))
     return img_Hh_Ww_c
 
 
