48bf1d2dc50b389cd33d79b09ca2aab9234abd06
khkim <kh.kim@medipixel.io>
2020-05-11 16:54:02 +0900

    Modify loss function for distillation

--- load_buffer.py
+++ load_buffer.py
@@ -1,6 +1,9 @@
 import pickle
 
-with open("data/distillation_buffer.pkl", "rb") as f:
+import numpy as np
+
+with open("data/distillation_buffer_pong_2e5.pkl", "rb") as f:
     memory = pickle.load(f)
 
 print(len(memory))
+print(np.mean(memory.q_value_buf))
