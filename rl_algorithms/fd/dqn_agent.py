458473d4dc6050505adfafa34daf87e0b094c573
mpbiorobot <kh.kim@medipixel.io>
2020-05-22 11:42:23 +0900

    Change argument of loss_fn

--- rl_algorithms/fd/dqn_agent.py
+++ rl_algorithms/fd/dqn_agent.py
@@ -78,14 +78,18 @@ class DQfDAgent(DQNAgent):
 
         # 1 step loss
         gamma = self.hyper_params.gamma
-        dq_loss_element_wise, q_values = self.loss_fn(experiences_1, gamma)
+        dq_loss_element_wise, q_values = self.loss_fn(
+            self.dqn, self.dqn_target, experiences_1, gamma, self.head_cfg
+        )
         dq_loss = torch.mean(dq_loss_element_wise * weights)
 
         # n step loss
         if self.use_n_step:
             experiences_n = self.memory_n.sample(indices)
             gamma = self.hyper_params.gamma ** self.hyper_params.n_step
-            dq_loss_n_element_wise, q_values_n = self.loss_fn(experiences_n, gamma)
+            dq_loss_n_element_wise, q_values_n = self.loss_fn(
+                self.dqn, self.dqn_target, experiences_n, gamma, self.head_cfg
+            )
 
             # to update loss and priorities
             q_values = 0.5 * (q_values + q_values_n)
