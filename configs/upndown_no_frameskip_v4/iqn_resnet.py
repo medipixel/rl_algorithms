a5658a119bf402028cfaa1784bdb6428645a935a
Leejin Jung <jin.prelude@gmail.com>
2020-05-28 15:16:41 +0900

    Add upndown env & configs

---
+++ configs/upndown_no_frameskip_v4/iqn_resnet.py
@@ -0,0 +1,72 @@
+"""Config for DQN on Pong-No_FrameSkip-v4.
+
+- Author: Kyunghwan Kim
+- Contact: kh.kim@medipixel.io
+"""
+from rl_algorithms.common.helper_functions import identity
+
+agent = dict(
+    type="DQNAgent",
+    hyper_params=dict(
+        gamma=0.99,
+        tau=5e-3,
+        buffer_size=int(1e4),  # openai baselines: int(1e4)
+        batch_size=16,  # openai baselines: 32
+        update_starts_from=int(1e4),  # openai baselines: int(1e4)
+        multiple_update=1,  # multiple learning updates
+        train_freq=4,  # in openai baselines, train_freq = 4
+        gradient_clip=10.0,  # dueling: 10.0
+        n_step=3,
+        w_n_step=1.0,
+        w_q_reg=0.0,
+        per_alpha=0.6,  # openai baselines: 0.6
+        per_beta=0.4,
+        per_eps=1e-6,
+        loss_type=dict(type="IQNLoss"),
+        # Epsilon Greedy
+        max_epsilon=0.0,
+        min_epsilon=0.0,  # openai baselines: 0.01
+        epsilon_decay=1e-6,  # openai baselines: 1e-7 / 1e-1
+        # grad_cam
+        grad_cam_layer_list=[
+            "backbone.layer1.0.conv2",
+            "backbone.layer2.0.shortcut.0",
+            "backbone.layer3.0.shortcut.0",
+            "backbone.layer4.0.shortcut.0",
+            "backbone.conv_out",
+        ],
+    ),
+    backbone=dict(
+        type="ResNet",
+        configs=dict(
+            use_bottleneck=False,
+            num_blocks=[1, 1, 1, 1],
+            block_output_sizes=[32, 32, 64, 64],
+            block_strides=[1, 2, 2, 2],
+            first_input_size=4,
+            first_output_size=32,
+            expansion=1,
+            channel_compression=4,  # output channel // channel_compression in last conv layer
+        ),
+    ),
+    head=dict(
+        type="IQNMLP",
+        configs=dict(
+            hidden_sizes=[512],
+            n_tau_samples=64,
+            n_tau_prime_samples=64,
+            n_quantile_samples=32,
+            quantile_embedding_dim=64,
+            kappa=1.0,
+            output_activation=identity,
+            # NoisyNet
+            use_noisy_net=True,
+            std_init=0.5,
+        ),
+    ),
+    optim_cfg=dict(
+        lr_dqn=1e-4,  # dueling: 6.25e-5, openai baselines: 1e-4
+        weight_decay=0.0,  # this makes saturation in cnn weights
+        adam_eps=1e-8,  # rainbow: 1.5e-4, openai baselines: 1e-8
+    ),
+)
---
+++ run_upndown_no_frameskip_v4.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""Train or test algorithms on UpNDownNoFrameskip-v4.
+
+- Author: Curt Park
+- Contact: curt.park@medipixel.io
+"""
+
+import argparse
+import datetime
+
+from rl_algorithms import build_agent
+from rl_algorithms.common.env.atari_wrappers import atari_env_generator
+import rl_algorithms.common.helper_functions as common_utils
+from rl_algorithms.utils import Config
+
+
+def parse_args() -> argparse.Namespace:
+    # configurations
+    parser = argparse.ArgumentParser(description="Pytorch RL algorithms")
+    parser.add_argument(
+        "--seed", type=int, default=777, help="random seed for reproducibility"
+    )
+    parser.add_argument(
+        "--cfg-path",
+        type=str,
+        default="configs/upndown_no_frameskip_v4/iqn.py",
+        help="config path",
+    )
+    parser.add_argument(
+        "--test", dest="test", action="store_true", help="test mode (no training)"
+    )
+    parser.add_argument(
+        "--grad-cam",
+        dest="grad_cam",
+        action="store_true",
+        help="test mode with viewing Grad-CAM",
+    )
+    parser.add_argument(
+        "--load-from",
+        type=str,
+        default=None,
+        help="load the saved model and optimizer at the beginning",
+    )
+    parser.add_argument(
+        "--off-render", dest="render", action="store_false", help="turn off rendering"
+    )
+    parser.add_argument(
+        "--render-after",
+        type=int,
+        default=0,
+        help="start rendering after the input number of episode",
+    )
+    parser.add_argument(
+        "--log", dest="log", action="store_true", help="turn on logging"
+    )
+    parser.add_argument(
+        "--save-period", type=int, default=100, help="save model period"
+    )
+    parser.add_argument(
+        "--episode-num", type=int, default=200000000, help="total episode num"
+    )
+    parser.add_argument(
+        "--max-episode-steps", type=int, default=None, help="max episode step"
+    )
+    parser.add_argument(
+        "--interim-test-num", type=int, default=10, help="interim test number"
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    """Main."""
+    args = parse_args()
+
+    # env initialization
+    env_name = "UpNDownNoFrameskip-v4"
+    env = atari_env_generator(env_name, args.max_episode_steps)
+
+    # set a random seed
+    common_utils.set_random_seed(args.seed, env)
+
+    # run
+    NOWTIMES = datetime.datetime.now()
+    curr_time = NOWTIMES.strftime("%y%m%d_%H%M%S")
+
+    cfg = Config.fromfile(args.cfg_path)
+    cfg.agent.log_cfg = dict(agent=cfg.agent.type, curr_time=curr_time)
+    build_args = dict(args=args, env=env)
+    agent = build_agent(cfg.agent, build_args)
+
+    if not args.test:
+        agent.train()
+    elif args.test and args.grad_cam:
+        agent.test_with_gradcam()
+    else:
+        agent.test()
+
+
+if __name__ == "__main__":
+    main()
