fdbc7eb64ef470b8af7e7951b6e17a0abd571e38
chris.yoon <chris.yoon@medipixel.io>
2020-06-09 13:31:05 +0900

    Add build_learner() to registry

--- rl_algorithms/registry.py
+++ rl_algorithms/registry.py
@@ -2,6 +2,7 @@ from rl_algorithms.utils import Registry, build_from_cfg
 from rl_algorithms.utils.config import ConfigDict
 
 AGENTS = Registry("agents")
+LEARNERS = Registry("learners")
 BACKBONES = Registry("backbones")
 HEADS = Registry("heads")
 LOSSES = Registry("losses")
@@ -11,6 +12,11 @@ HERS = Registry("hers")
 def build_agent(cfg: ConfigDict, build_args: dict = None):
     """Build agent using config and additional arguments."""
     return build_from_cfg(cfg, AGENTS, build_args)
+
+
+def build_learner(cfg: ConfigDict, build_args: dict = None):
+    """Build learner using config and additional arguments."""
+    return build_from_cfg(cfg, LEARNERS, build_args)
 
 
 def build_backbone(cfg: ConfigDict, build_args: dict = None):
