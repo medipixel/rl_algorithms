24ca41792c02ade2e76a8ce37e785c16da59dc31
chris.yoon <chris.yoon@medipixel.io>
2020-06-09 10:19:11 +0900

    Create base class for distributed architectures

---
+++ rl_algorithms/common/distributed/apex/architecture.py
@@ -0,0 +1,12 @@
+# # from rl_algorithms.common.distributed.abstract.architecture import Architecture
+
+
+# class ApeX(Architecture):
+#     def __init__(self):
+#         pass
+
+#     def spawn(self):
+#         pass
+
+#     def train(self):
+#         pass
