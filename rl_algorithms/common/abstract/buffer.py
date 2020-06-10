3a5ae7f6cdf28b10b42e738d820e2b0a100305c6
Leejin Jung <jin.prelude@gmail.com>
2020-06-10 14:00:22 +0900

    Add descriptions & Change config parameter

--- rl_algorithms/common/abstract/buffer.py
+++ rl_algorithms/common/abstract/buffer.py
@@ -1,20 +1,33 @@
+# -*- coding: utf-8 -*-
+"""Abstract Buffer & BufferWrapper class.
+- Author: Euijin Jeong
+- Contact: euijin.jeong@medipixel.io
+"""
+
 from typing import Any, Tuple
 
 import numpy as np
 
 
 class Buffer:
+    """Abstract Buffer used for replay buffer."""
+
     def add(self, transition: Tuple[Any, ...]) -> Tuple[Any, ...]:
-        pass
+        """Add a new experience to memory."""
 
     def sample(self) -> Tuple[np.ndarray, ...]:
-        pass
+        """Sample a batch of experiences from memory."""
 
     def __len__(self) -> int:
-        pass
+        """Return the current size of internal memory."""
 
 
 class BufferWrapper(Buffer):
+    """Abstract BufferWrapper used for buffer wrapper.
+    Attributes:
+        buffer (Buffer): Hold replay buffer as am attribute.
+    """
+
     def __init__(self, replay_buffer: Buffer):
         self.buffer = replay_buffer
 
@@ -25,4 +38,5 @@ class BufferWrapper(Buffer):
         return self.buffer.sample()
 
     def __len__(self) -> int:
+        """Return the current size of internal memory."""
         return len(self.buffer)
