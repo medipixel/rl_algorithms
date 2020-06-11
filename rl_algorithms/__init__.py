cfd194eeb2b02d15c3d67a6d8daa60a2e1fb9616
Leejin Jung <jin.prelude@gmail.com>
2020-06-11 18:04:43 +0900

    Fix issues commented

--- rl_algorithms/__init__.py
+++ rl_algorithms/__init__.py
@@ -20,6 +20,8 @@ from .fd.sac_learner import SACfDLearner
 from .ppo.agent import PPOAgent
 from .ppo.learner import PPOLearner
 from .recurrent.dqn_agent import R2D1Agent
+from .recurrent.learner import R2D1Learner
+from .recurrent.losses import R2D1C51Loss, R2D1DQNLoss, R2D1IQNLoss
 from .registry import build_agent, build_her
 from .sac.agent import SACAgent
 from .sac.learner import SACLearner
@@ -50,6 +52,7 @@ __all__ = [
     "PPOLearner",
     "SACLearner",
     "TD3Learner",
+    "R2D1Learner",
     "LunarLanderContinuousHER",
     "ReacherHER",
     "build_agent",
@@ -59,4 +62,7 @@ __all__ = [
     "IQNLoss",
     "C51Loss",
     "DQNLoss",
+    "R2D1IQNLoss",
+    "R2D1C51Loss",
+    "R2D1DQNLoss",
 ]
