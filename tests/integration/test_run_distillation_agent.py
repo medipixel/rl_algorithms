8fa68ebddcb2c2d22dc0724cde34d29b43738b80
Euijin Jeong <euijin.jeong@medipixel.io>
2020-09-15 15:05:50 +0900

    Add n_frame_from_last test code & Add exception code

--- tests/integration/test_run_distillation_agent.py
+++ tests/integration/test_run_distillation_agent.py
@@ -7,7 +7,8 @@ import shutil
 import subprocess
 
 
-def check_train_phase_data_generating(config: str, run_file: str):
+def check_distillation_agent(config: str, run_file: str):
+    """Test that 1 episode of run file works well."""
     cmd = (
         f"python {run_file} --cfg-path {config} --integration-test "
         + f"--episode-num 1 --interim-test 1 --off-render"
@@ -24,38 +25,43 @@ def check_train_phase_data_generating(config: str, run_file: str):
     print(str(output))
     assert p.returncode == 0
 
-    # Find saved checkpoint path
+    # Find saved checkpoint path and data path.
     pattern = r"./checkpoint/.+/"
     data_pattern = r"./data/.+/"
     checkpoint_path = re.findall(pattern, str(output))[0]
+    full_data_path, n_frame_from_last_path = re.findall(data_pattern, str(output))
 
-    # check if data saved properly
-    num_episode_step = re.findall(r"episode step: \d+", str(output))[0]
-    num_episode_step = int(re.findall(r"\d+", num_episode_step)[0])
+    try:
+        num_episode_step = re.findall(r"episode step: \d+", str(output))[0]
+        num_episode_step = int(re.findall(r"\d+", num_episode_step)[0])
 
-    # check if the number of data is same with iterated episode step
-    data_path = re.findall(data_pattern, str(output))[0]
-    saved_data_list = os.listdir(data_path)
-    assert (
-        len(saved_data_list) == num_episode_step
-    ), "The number of data does not match the number of iterated episode steps."
+        # Check if the number of data is same with iterated episode step.
+        saved_data_list = os.listdir(full_data_path)
+        assert (
+            len(saved_data_list) == num_episode_step
+        ), "The number of data does not match the number of iterated episode steps."
 
-    with open(data_path + saved_data_list[0], "rb") as f:
-        datum = pickle.load(f)
+        # Check if n_frame_from_last works well.
+        n_frame_from_last_data_list = os.listdir(n_frame_from_last_path)
+        assert 3 == len(
+            n_frame_from_last_data_list
+        ), f"n_frame_from_last doesn't work properly(expected num of data: 3, num of data: {len(n_frame_from_last_data_list)})."
 
-    assert (
-        len(datum) == 1
-    ), "The length of the data is not appropriate(length must be 1, state only)."
+        # Check if train-phase data only contaions state, not state & q value.
+        with open(full_data_path + saved_data_list[0], "rb") as f:
+            datum = pickle.load(f)
+        assert (
+            len(datum) == 1
+        ), "The length of the data is not appropriate(length must be 1, state only)."
 
-    return checkpoint_path, data_path
+    except Exception as e:
+        raise e
 
-
-def check_distillation_agent(config: str, run_file: str):
-    """Test that 1 episode of run file works well."""
-
-    checkpoint_path, data_path = check_train_phase_data_generating(config, run_file)
-    check_save_path(checkpoint_path)
-    check_save_path(data_path)
+    finally:
+        """Delete generated directories."""
+        delete_path(checkpoint_path)
+        delete_path(full_data_path)
+        delete_path(n_frame_from_last_path)
 
 
 def check_save_path(save_path: str):
@@ -63,12 +69,16 @@ def check_save_path(save_path: str):
     assert os.path.exists(save_path)
 
     # Remove checkpoint dir
-    shutil.rmtree(save_path)
+    delete_path(save_path)
+
+
+def delete_path(path: str):
+    """Remove directory."""
+    shutil.rmtree(path)
 
 
 def test_distillation():
-    """Test distillation agent"""
-
+    """Test distillation agent."""
     check_distillation_agent(
         "configs/pong_no_frameskip_v4/distillation_dqn.py",
         "run_pong_no_frameskip_v4.py",
