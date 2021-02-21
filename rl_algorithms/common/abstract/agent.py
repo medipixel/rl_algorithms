33140e5f7828da50b0f5014c10f721ac40dafbc3
khkim <kh.kim@medipixel.io>
2021-02-21 16:20:55 +0900

    Fix print format

--- rl_algorithms/common/abstract/agent.py
+++ rl_algorithms/common/abstract/agent.py
@@ -134,7 +134,7 @@ class Agent(ABC):
                 step += 1
 
             print(
-                "[INFO] test %d\tstep: %d\ttotal score: %d" % (i_episode, step, score)
+                "[INFO] test %d\tstep: %d\ttotal score: %4f" % (i_episode, step, score)
             )
             score_list.append(score)
 
