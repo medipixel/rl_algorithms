7a25205876ed67699d15a34acb4a2468ad8caae6
khkim <kh.kim@medipixel.io>
2021-03-03 16:06:35 +0900

    Fix typo

--- tools/run_descrete_env.sh
+++ tools/run_descrete_env.sh
@@ -1,9 +1,9 @@
-python run_lunarlander_v2.py --cfg-path ./configs/lunarlander_v2/dqn.py --off-render --log
-python run_lunarlander_v2.py --cfg-path ./configs/lunarlander_v2/dqfd.py --off-render --log
-python run_lunarlander_v2.py --cfg-path ./configs/lunarlander_v2/r2d1.py --off-render --log
+python run_lunarlander_v2.py --cfg-path ./configs/lunarlander_v2/dqn.yaml --off-render --log
+python run_lunarlander_v2.py --cfg-path ./configs/lunarlander_v2/dqfd.yaml --off-render --log
+python run_lunarlander_v2.py --cfg-path ./configs/lunarlander_v2/r2d1.yaml --off-render --log
 
-python run_pong_no_frameskip_v4.py --cfg-path ./configs/pong_no_frameskip_v4/dqn.py --off-render --log
-python run_pong_no_frameskip_v4.py --cfg-path ./configs/pong_no_frameskip_v4/r2d1.py --off-render --log
-python run_pong_no_frameskip_v4.py --cfg-path configs/pong_no_frameskip_v4/apex_dqn.py --off-render --log
+python run_pong_no_frameskip_v4.py --cfg-path ./configs/pong_no_frameskip_v4/dqn.yaml --off-render --log
+python run_pong_no_frameskip_v4.py --cfg-path ./configs/pong_no_frameskip_v4/r2d1.yaml --off-render --log
+python run_pong_no_frameskip_v4.py --cfg-path configs/pong_no_frameskip_v4/apex_dqn.yaml --off-render --log
 
-python run_pong_no_frameskip_v4.py --cfg-path ./configs/pong_no_frameskip_v4/dqn_resnet.py --off-render --log
+python run_pong_no_frameskip_v4.py --cfg-path ./configs/pong_no_frameskip_v4/dqn_resnet.yaml --off-render --log
--- tools/run_reacher_v2.sh
+++ tools/run_reacher_v2.sh
@@ -1,5 +1,5 @@
-python run_reacher_v2.py --cfg-path ./configs/reacher_v2/ddpg.py --off-render --log
-python run_reacher_v2.py --cfg-path ./configs/reacher_v2/sac.py --off-render --log
-python run_reacher_v2.py --cfg-path ./configs/reacher_v2/td3.py --off-render --log
-python run_reacher_v2.py --cfg-path ./configs/reacher_v2/bc_ddpg.py --off-render --log
-python run_reacher_v2.py --cfg-path ./configs/reacher_v2/bc_sac.py --off-render --log
+python run_reacher_v2.py --cfg-path ./configs/reacher_v2/ddpg.yaml --off-render --log
+python run_reacher_v2.py --cfg-path ./configs/reacher_v2/sac.yaml --off-render --log
+python run_reacher_v2.py --cfg-path ./configs/reacher_v2/td3.yaml --off-render --log
+python run_reacher_v2.py --cfg-path ./configs/reacher_v2/bc_ddpg.yaml --off-render --log
+python run_reacher_v2.py --cfg-path ./configs/reacher_v2/bc_sac.yaml --off-render --log
